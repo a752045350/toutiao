9d6c3cf5ebfb26f9748546e14ae24c00bd9f5dec
typ <397815397@qq.com>
2022-10-09 12:10:22 +0800

    项目初始化

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <div class="app">
+<van-button type="primary">主要按钮</van-button>
+<van-button type="info">信息按钮</van-button>
+<van-button type="default">默认按钮</van-button>
+<van-button type="warning">警告按钮</van-button>
+<van-button type="danger">危险按钮</van-button>
+
   <router-view></router-view>
 
   </div>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,15 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+import '@/styles/index.less'
+
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+import 'amfe-flexible'
+
+Vue.use(Vant)
+
 new Vue({
   router,
   store,
